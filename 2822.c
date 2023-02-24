3afeb491420e623bd0b8605d059a17efb468de09
junhyeok <pjh3684@naver.com>
2023-02-24 21:34:16 +0900

    first commit

---
+++ 2822.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(void){
+
+    int a;
+    printf("\n");
+    scanf("%d",&a);
+    printf("%d",a);
+
+}
---
+++ hello_world.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void){
+
+    printf("hello world!\n");
+    printf("hello gcc");
+
+    return 0;
+}
