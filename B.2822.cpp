442d90eb82259d300511d0921392d3cbdb2f02a6
junhyeok <pjh3684@naver.com>
2023-02-25 18:36:24 +0900

    3

--- B.2822.cpp
+++ B.2822.cpp
@@ -1,11 +1,15 @@
+#include <vector>
 #include <iostream>
 
 using namespace std;
 int main(void){
-   int a = 0; 
-   cout <<"박준혁" <<endl; 
-   cin >> a; 
-   cout << a <<endl; 
-   cout << a+a <<endl;
 
- }
+  vector<int> v;
+  v.push_back(10);
+  v.push_back(20);
+  v.push_back(30);
+  cout << "벡터의 크기 : " << v.size() << endl;
+  cout << "첫 번째의 원소: " << v[0] << endl;
+  cout << "두 번째의 원소: " << v[1] << endl;
+  cout << "세 번째의 원소:"  << v[2] << endl;
+  }
