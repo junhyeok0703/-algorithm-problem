fe2b65b33cee0b7f9ecf1b4d7ae97f85767ea9fa
junhyeok <pjh3684@naver.com>
2023-02-25 16:50:30 +0900

    다시설정

---
+++ B.2822.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main(void){
+    
+   std::cout << "hello world" << std::endl;
+   std::cout << "hello worl222!!" << std::endl;
+   std::cout << "hello worl222!!" << std::endl;
+
+}
--- hello_world.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void){
-
-    printf("hello world!\n");
-    printf("hello gcc");
-
-    return 0;
-}
